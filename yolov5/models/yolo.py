779e209fa78b3f1ebd1e09c34f279fc135bf0edd
Arth Sonani <115365732+ArthSonani@users.noreply.github.com>
2025-09-18 23:03:08 +0530

    Update yolo.py

--- yolov5/models/yolo.py
+++ yolov5/models/yolo.py
@@ -19,7 +19,7 @@ import torch
 import torch.nn as nn
 
 FILE = Path(__file__).resolve()
-ROOT = FILE.parents[1]  # YOLOv5 root directory
+ROOT = FILE.parents[1]  
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 if platform.system() != "Windows":
